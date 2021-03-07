011d670afbad262f6e8ec59ab8fe4a34368ef495
James Galantino <james_galantino@aol.com>
2021-03-07 18:46:23 -0500

    Fixed bug regarding onsuccess function, saveRecord function

--- public/db.js
+++ public/db.js
@@ -9,13 +9,46 @@ request.onupgradeneed = function(event) {
 
 request.onsuccess = function(event) {
     db = event.target.result;
-};
 
-if (navigator.onLine) {
-    checkDatabase();
+    if (navigator.onLine) {
+        checkDatabase();
+    };
 };
 
 request.onerror = function(event) {
     console.log("Something went wrong! " + event.target.errorCode);
 };
 
+function saveRecord(record) {
+    const transcation = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+};
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+
+                store.clear();
+            });
+        };
+    };
+};
+
+window.addEventListener("online", checkDatabase);
